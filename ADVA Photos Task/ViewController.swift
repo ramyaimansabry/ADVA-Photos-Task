11c3ff9084d9b2a4b4c1b0ee6d4ddc642547f3e9
Ramy Sabry <ramy.sabry.external@atos.net>
2022-03-28 20:37:21 +0200

    init

---
+++ ADVA Photos Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ADVA Photos Task
+//
+//  Created by Ramy Sabry on 28/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
