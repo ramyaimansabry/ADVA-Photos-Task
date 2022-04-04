f630c4acff70f73e7e34c42655bffaa327e90817
Ramy Sabry <ramy.sabry.external@atos.net>
2022-04-04 13:52:58 +0200

    photo details image zooming

--- ADVA Photos Task/Home Module/Photo Details Screen/Presentation/View/PhotoDetailsViewViewController.swift
+++ ADVA Photos Task/Home Module/Photo Details Screen/Presentation/View/PhotoDetailsViewViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import SDWebImage
 
 class PhotoDetailsViewViewController: BaseViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var photoImageView: UIImageView!
     
     private var viewModel: PhotoDetailsViewModel
@@ -24,8 +25,8 @@ class PhotoDetailsViewViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        photoImageView.sd_setImage(with: URL(string: viewModel.photoURL.value), completed: nil)
-        photoImageView.enableZoom()
+        setupScrollView()
+        loadPhotoImage()
     }
 
     @IBAction func onPressBackButton(_ sender: UIButton) {
@@ -33,18 +34,50 @@ class PhotoDetailsViewViewController: BaseViewController {
     }
 }
 
-extension UIImageView {
-  func enableZoom() {
-    let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming(_:)))
-    isUserInteractionEnabled = true
-    addGestureRecognizer(pinchGesture)
-  }
+// MARK: - Private Methods
 
-  @objc
-  private func startZooming(_ sender: UIPinchGestureRecognizer) {
-    let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
-    guard let scale = scaleResult, scale.a > 1, scale.d > 1 else { return }
-    sender.view?.transform = scale
-    sender.scale = 1
-  }
+private extension PhotoDetailsViewViewController {
+    func setupScrollView() {
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 10.0
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+    func loadPhotoImage() {
+        photoImageView.sd_setImage(with: URL(string: viewModel.photoURL.value), completed: nil)
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension PhotoDetailsViewViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoImageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        guard
+            let image = photoImageView.image,
+            scrollView.zoomScale > 1
+        else {
+            scrollView.contentInset = .zero
+            return
+        }
+        
+        let ratioW = photoImageView.frame.width / image.size.width
+        let ratioH = photoImageView.frame.height / image.size.height
+        
+        let ratio = ratioW < ratioH ? ratioW : ratioH
+        let newWidth = image.size.width * ratio
+        let newHeight = image.size.height * ratio
+        let conditionLeft = newWidth*scrollView.zoomScale > photoImageView.frame.width
+        let left = 0.5 * (conditionLeft ? newWidth - photoImageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+        let conditioTop = newHeight*scrollView.zoomScale > photoImageView.frame.height
+        
+        let top = 0.5 * (conditioTop ? newHeight - photoImageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+        
+        scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+    }
 }
